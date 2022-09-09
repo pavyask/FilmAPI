ee1e8b250a6080b60c00c719bf55ae97421ea127
pavyask <pavel.yasko03@gmail.com>
2022-09-09 15:59:25 +0200

    Added Film model

---
+++ FilmAPI/Film.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmAPI
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public float Rating { get; set; } 
+    }
+}

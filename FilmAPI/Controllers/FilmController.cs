b7e49a23208cc294bb2a25481dd4c52e1a22bf8b
pavyask <pavel.yasko03@gmail.com>
2022-09-09 18:28:56 +0200

    Created DataContext; created SqlServer database and established connection; reworked CRUD methods to operate on database

--- FilmAPI/Controllers/FilmController.cs
+++ FilmAPI/Controllers/FilmController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FilmAPI.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FilmAPI.Controllers
 {
@@ -7,23 +9,23 @@ namespace FilmAPI.Controllers
     [ApiController]
     public class FilmController : ControllerBase
     {
-        private static List<Film> films = new List<Film>
-            {
-                new Film{Id=1,Title="Interstellar", Year=2014 ,Rating=8.6F},
-                new Film{Id=2,Title="The Lord of the Rings: The Fellowship of the Ring", Year=2001 ,Rating=8.8F},
-                new Film{Id=3,Title="Inception", Year=2010 ,Rating=8.8F}
-            };
+        private readonly DataContext _context;
+
+        public FilmController(DataContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Film>>> GetFilms()
         {
-            return Ok(films);
+            return Ok(await _context.Film.ToListAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Film>> GetFilm(int id)
         {
-            var film = films.Find(x => x.Id == id);
+            var film = await _context.Film.FindAsync(id);
             if (film == null)
                 return NotFound("Film not found");
 
@@ -33,32 +35,39 @@ namespace FilmAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<List<Film>>> AddFilm(Film film)
         {
-            films.Add(film);
-            return Ok(films);
+            _context.Film.Add(film);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Film.ToListAsync());
         }
 
         [HttpPut]
-        public async Task<ActionResult<List<Film>>> UpdateFilm(Film film)
+        public async Task<ActionResult<List<Film>>> UpdateFilm(Film request)
         {
-            var filmToChange = films.Find(x => x.Id == film.Id);
-            if (filmToChange == null)
+            var dbFilm = await _context.Film.FindAsync(request.Id);
+            if (dbFilm == null)
                 return NotFound("There is no film with such id");
 
-            filmToChange.Year = film.Year;
-            filmToChange.Title = film.Title;
-            filmToChange.Rating = film.Rating;
-            return Ok(films);
+            dbFilm.Year = request.Year;
+            dbFilm.Title = request.Title;
+            dbFilm.Rating = request.Rating;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Film.ToListAsync());
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Film>> DeleteFilm(int id)
         {
-            var film = films.Find(x => x.Id == id);
-            if (film == null)
+            var dbFilm = await _context.Film.FindAsync(id);
+            if (dbFilm == null)
                 return NotFound("There is no film with such id");
 
-            films.Remove(film);
-            return Ok(film);
+            _context.Film.Remove(dbFilm);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Film.ToListAsync());
         }
 
     }
---
+++ FilmAPI/Migrations/20220909155151_CreateInitial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmAPI.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Film",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Film", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Film");
+        }
+    }
+}

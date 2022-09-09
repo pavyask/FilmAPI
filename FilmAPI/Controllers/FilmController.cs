76d38fb18e1c10790cac2c2035aa332d81aa093e
pavyask <pavel.yasko03@gmail.com>
2022-09-09 17:14:35 +0200

    Added other CRUD methods for FilmController

--- FilmAPI/Controllers/FilmController.cs
+++ FilmAPI/Controllers/FilmController.cs
@@ -15,9 +15,51 @@ namespace FilmAPI.Controllers
             };
 
         [HttpGet]
-        public async Task<ActionResult<List<Film>>> Get()
+        public async Task<ActionResult<List<Film>>> GetFilms()
         {
             return Ok(films);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Film>> GetFilm(int id)
+        {
+            var film = films.Find(x => x.Id == id);
+            if (film == null)
+                return NotFound("Film not found");
+
+            return Ok(film);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Film>>> AddFilm(Film film)
+        {
+            films.Add(film);
+            return Ok(films);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Film>>> UpdateFilm(Film film)
+        {
+            var filmToChange = films.Find(x => x.Id == film.Id);
+            if (filmToChange == null)
+                return NotFound("There is no film with such id");
+
+            filmToChange.Year = film.Year;
+            filmToChange.Title = film.Title;
+            filmToChange.Rating = film.Rating;
+            return Ok(films);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Film>> DeleteFilm(int id)
+        {
+            var film = films.Find(x => x.Id == id);
+            if (film == null)
+                return NotFound("There is no film with such id");
+
+            films.Remove(film);
+            return Ok(film);
+        }
+
     }
 }
